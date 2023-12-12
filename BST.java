8457a2d0d50d213198ee321b1d617d242bef2295
Rhys Walker <rhys.h.walker@gmail.com>
2023-12-12 22:46:24 +0000

    First Release of BST and Node

---
+++ BST.java
@@ -0,0 +1,180 @@
+/**
+ * An implementation of a Binary Search Tree.
+ * 
+ * @author Rhys Walker
+ * @version 1.1
+ * @since 2023-12-12
+ */
+
+import java.util.ArrayList;
+
+public class BST {
+    private Node root;
+    public BST(){
+        root = null;
+    }
+
+    /**
+     * Insert an item into the tree
+     * @param data An int that we want to store
+     */
+    public void addItem(int data){
+        if (root == null){
+            root = new Node(data, null);
+        }
+        else {
+            insert(data);
+        }
+    }
+
+    /**
+     * A getter function to return the root node of the tree
+     * @return The root node of the tree
+     */
+    public Node root(){
+        return root;
+    }
+
+    /**
+     * Called from the public function to insert an item into the tree
+     * @param data The int we want to insert
+     * @return The root node of the tree
+     */
+    private Node insert(int data){
+        Node currentNode = root;
+        while(true){
+            if(data > currentNode.getData()){ // going right
+                if (currentNode.getRight() == null){ // we have found our position
+                    currentNode.setRight(new Node(data, currentNode)); // set right child to be new node
+                    return root;
+                }
+                else{
+                    currentNode = currentNode.getRight(); // set to right as that the way we are going
+                }
+            }
+            else if(data < currentNode.getData()){ // going left
+                if (currentNode.getLeft() == null){ // we have found our position
+                    currentNode.setLeft(new Node(data, currentNode)); // set right child to be new node
+                    return root;
+                }
+                else{
+                    currentNode = currentNode.getLeft(); // set to right as that the way we are going
+                }
+            }
+            else { // node is duplicate and not allowed
+                return root;
+            }
+        }
+    }
+
+    /**
+     * Finds the node from given data
+     * @param data The data in the node we are looking for
+     * @return the node that we are looking for, null if can't be found
+     */
+    public Node findNode(int data){
+        Node currentNode = root;
+
+        while(true){
+            if (currentNode != null){ // null check
+
+                if (data > currentNode.getData()){ // go right
+                    currentNode = currentNode.getRight();
+                }
+
+                else if (data < currentNode.getData()){ // go left
+                    currentNode = currentNode.getLeft();
+                }
+
+                else if (data == currentNode.getData()){ // if not more or less then we know we have found our node
+                    return currentNode;
+                }
+            }
+
+            else { // if null then node isn't in tree
+                return null; // not in the tree
+            }
+        }
+    }
+
+    /**
+     * Produces a list of items to the item we are looking for
+     * @param data Value of what we are looking for
+     * @return ArrayList of values to node
+     */
+    public ArrayList<Integer> listToNode(int data){
+        Node currentNode = root;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        while(true){
+            if (currentNode != null){ // make sure node is not null
+
+                list.add(currentNode.getData());
+
+                if (data > currentNode.getData()){ // go right
+                    currentNode = currentNode.getRight();
+                }
+
+                else if (data < currentNode.getData()){ // go left
+                    currentNode = currentNode.getLeft();
+                }
+
+                else if (data == currentNode.getData()){ // if not more or less then we know we have found our node
+                    return list;
+                }
+            }
+            else { // if node is null then node we are searching for isnt in tree
+                return null;
+            }
+        }
+    }
+
+    /**
+     * Traverse the tree using in order traversal
+     * @return An arraylist of the nodes in order
+     */
+    public ArrayList<Node> inOrderTraversal(){
+
+        // create the arraylist that is going to house the traversal
+        ArrayList<Node> inOrderList = new ArrayList<Node>();
+
+        inOrderList = checkNodeIO(root, inOrderList); // set the list equal to the returned
+        return inOrderList; 
+    }
+
+    /**
+     * Function to recursively check the node in order
+     * @param node The node we are currently on
+     * @param inOrderList The list to add to
+     * @return ArrayList of in order
+     */
+    private ArrayList<Node> checkNodeIO(Node node, ArrayList<Node> inOrderList){
+
+        // if null we have reached a leaf node
+        if (node == null){
+            return inOrderList;
+        }
+
+        inOrderList = checkNodeIO(node.getLeft(), inOrderList); // recursively check left
+
+        inOrderList.add(node); // add the current node to the list
+
+        inOrderList = checkNodeIO(node.getRight(), inOrderList); // recursively check right
+
+        return inOrderList; // return the list
+    }
+
+    /**
+     * Finds the parent of a given node
+     * @param child The node whose parent we want to find
+     * @return
+     */
+    public Node findParentNode(int childData){
+
+        // get a list of nodes
+        ArrayList<Integer> list = listToNode(childData);
+
+        Node parent = findNode(list.get(list.size()-2));
+
+        return parent;
+    }
+}
---
+++ Node.java
@@ -0,0 +1,83 @@
+/**
+ * A basic implementation of a node in a binary search tree. It is only compatible with integers.
+ * @author Rhys Walker
+ * @version 1.1
+ * @since 2023-12-11
+ */
+
+ // javadoc Node.java
+
+public class Node {
+    private int data;
+    private Node right;
+    private Node left;
+    private Node parent;
+
+    /**
+     * Constructor for a node in out binary tree
+     * @param data The data to be stored in a given node
+     * @param parent The parent node of our node. Will be null if is root node
+     */
+    public Node(int data, Node parent){
+        this.data = data;
+        this.parent = parent;
+        this.right = null;
+        this.left = null;
+    }
+
+    /**
+     * Get the data from the node
+     * @return The data from the node
+     */
+    public int getData(){
+        return data;
+    }
+
+    /**
+     * Set the parent pointer for the node
+     * @param newParent The node that will be the new parent
+     */
+    public void changeParent(Node newParent){
+        this.parent = newParent;
+    }
+
+    /**
+     * Set the left pointer
+     * @param left The node that will be left child
+     */
+    public void setLeft(Node left){
+        this.left = left;
+    }
+
+    /**
+     * Set the right pointer
+     * @param right The node that will be right child
+     */
+    public void setRight(Node right){
+        this.right = right;
+    }
+
+    /**
+     * Gets the parent of our node
+     * @return The parent of our node
+     */
+    public Node getParent(){
+        return parent;
+    }
+
+    /**
+     * Gets the right child of our node
+     * @return The right child of our node
+     */
+    public Node getRight(){
+        return right;
+    }
+
+    /**
+     * Gets the left child of our node
+     * @return The left child of our node
+     */
+    public Node getLeft(){
+        return left;
+    }
+}
